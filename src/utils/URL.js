61041e9d725ea2c8783c6f1959a4870bc7f5e15d
Majomon <mauri.monzon91@gmail.com>
2023-07-14 15:31:25 -0300

    Update

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,2 +1,2 @@
 // export const URL_BASE = "http://localhost:3001";
-export const URL_BASE = "https://pokemon-api-liart.vercel.app";
+export const URL_BASE = "https://pokemon-api-two-inky.vercel.app/";
